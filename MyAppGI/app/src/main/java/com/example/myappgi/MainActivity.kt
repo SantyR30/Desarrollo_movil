a6aeb628de5025c75612e8c6b1a6d711b8b90a61
SANTIAGO RAMIREZ MALAGON <santyramirez0330@gmail.com>
2024-05-26 12:29:32 -0500

    Se agrego los nuevos modulos registro y la barra de navegaion adecuando tambien unas imagenes

--- MyAppGI/app/src/main/java/com/example/myappgi/MainActivity.kt
+++ MyAppGI/app/src/main/java/com/example/myappgi/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.myappgi
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
-import androidx.core.view.WindowCompat
 
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+/*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,4 +60,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
+*/
